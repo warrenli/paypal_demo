173efdc3ea192c9e6cfc853b80c386557b7b9d5f
Warren Li <warrenli@dontrush.org>
2008-12-16 20:36:57 +0800

    Fixed minor bugs in views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_filter :clear_session_data
+
   def index
   end
+  private
 
+  def clear_session_data
+    session[:ec_response] = nil
+    session[:ec_final] = nil
+    session[:pp_errors] = nil
+  end
 end
